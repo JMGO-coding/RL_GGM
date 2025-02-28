1ada459bb0430806c2162c378e73957540a98738
AnaGM2 <anagilmolina2@gmail.com>
2025-02-28 21:08:59 +0100

    Update __init__.py

--- src/policies/__init__.py
+++ src/policies/__init__.py
@@ -1,7 +1,6 @@
 # Importación de módulos o clases
-from .epsilon_soft import EpsilonSoftPolicy
 from .epsilon_greedy import EpsilonGreedyPolicy
-from .greedy import GreedyPolicy
+from .greedy import GreedyFromQPolicy
 
 # Lista de módulos o clases públicas
-__all__ = ['EpsilonSoftPolicy', 'EpsilonGreedyPolicy', 'GreedyPolicy']
+__all__ = ['EpsilonGreedyPolicy', 'GreedyFromQPolicy']
