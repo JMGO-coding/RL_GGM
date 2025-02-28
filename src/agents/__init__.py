701c0d1ef14b8f7d3a0526495f14c283550d5fae
JMGO-coding <gochema00@gmail.com>
2025-02-28 18:06:59 +0100

    Update __init__.py

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,1 +1,10 @@
+# Importación de módulos o clases
+from .agent1 import Agent
+from .epsilon_greedy import EpsilonGreedy
+from .ucb1 import UCB1
+from .ucb2 import UCB2
+from .softmax import Softmax
+from .gradiente_preferencias import GradienteDePreferencias
 
+# Lista de módulos o clases públicas
+__all__ = ['Agent', 'EpsilonGreedy', 'UCB1', 'UCB2', 'Softmax', 'GradienteDePreferencias']
