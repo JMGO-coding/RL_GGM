145d6031a32c22108e5ee3c9690a04d6863c6a3b
AnaGM2 <anagilmolina2@gmail.com>
2025-03-02 00:34:53 +0100

    Update on_policy_MC_all_visits_epsilonGreedy_over_epsilonsoft.py

--- src/agents/on_policy_MC_all_visits_epsilonGreedy_over_epsilonsoft.py
+++ src/agents/on_policy_MC_all_visits_epsilonGreedy_over_epsilonsoft.py
@@ -26,7 +26,7 @@ class AgentMCOnPolicyAllVisits(Agent):
         self.Q = np.zeros([env.observation_space.n, self.nA])
         self.n_visits = np.zeros([env.observation_space.n, self.nA])
         self.returns = np.zeros([env.observation_space.n, self.nA])
-        self.epsilon_soft_policy = EpsilonSoft(epsilon=self.epsilon, nA=self.nA)
+        self.epsilon_soft_policy = EpsilonSoftPolicy(epsilon=self.epsilon, nA=self.nA)
         self.stats = 0.0
         self.list_stats = []
         self.episode_lengths = []    # Lista para almacenar las longitudes de los episodios
