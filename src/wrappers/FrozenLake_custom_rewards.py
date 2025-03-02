59a054f18c6229573501fa1d50d800c5a1cc8a99
JMGO-coding <gochema00@gmail.com>
2025-03-02 13:26:08 +0100

    Update FrozenLake_custom_rewards.py

--- src/wrappers/FrozenLake_custom_rewards.py
+++ src/wrappers/FrozenLake_custom_rewards.py
@@ -1,7 +1,6 @@
 import gymnasium as gym
-from gymnasium.wrappers import Wrapper
 
-class FrozenLakeCustomRewards(Wrapper):
+class FrozenLakeCustomRewards(gym.Wrapper):
     def __init__(self, env, hole_penalty=-1.0):
         super().__init__(env)
         self.hole_penalty = hole_penalty  # Penalización por caer en un agujero
