f5710530fe19fc15ba9e815fdeb665a4f0a09635
JMGO-coding <gochema00@gmail.com>
2025-03-02 13:08:33 +0100

    Update FrozenLake_custom_rewards.py

--- src/wrappers/FrozenLake_custom_rewards.py
+++ src/wrappers/FrozenLake_custom_rewards.py
@@ -5,12 +5,13 @@ class FrozenLakeCustomRewards(RewardWrapper):
     def __init__(self, env, hole_penalty=-1.0):
         super().__init__(env)
         self.hole_penalty = hole_penalty  # Penalización por caer en un agujero
+        self.goal_state = self.env.observation_space.n - 1
     
     def step(self, action):
         next_state, reward, terminated, truncated, info = self.env.step(action)
         
         # Si el episodio termina y la recompensa es 0, significa que caímos en un agujero
         if done and next_state != self.goal_state:
-            reward = -1  # Penalización por fallar
+            reward = self.hole_penalty  # Penalización por fallar
 
         return next_state, reward, terminated, truncated, info
