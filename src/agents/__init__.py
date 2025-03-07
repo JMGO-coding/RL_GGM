d08e0de8696e59ee128b28ee9cb21a311512f924
AnaGM2 <anagilmolina2@gmail.com>
2025-03-07 17:53:39 +0100

    Update __init__.py

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,6 +1,8 @@
 # Importación de módulos o clases
 from .agent import Agent
-from .on_policy_MC_all_visits_epsilonGreedy_over_epsilonsoft import AgentMCOnPolicyAllVisits
+from .on_policy_MC_all_visits import AgentMCOnPolicyAllVisits
+from .off_policy_MC_all_visits import AgentMCOffPolicyAllVisits
+from .SARSA import AgentSARSA
 
 # Lista de módulos o clases públicas
-__all__ = ['Agent', 'AgentMCOnPolicyAllVisits']
+__all__ = ['Agent', 'AgentMCOnPolicyAllVisits', 'AgentMCOffPolicyAllVisits', 'AgentSARSA']
