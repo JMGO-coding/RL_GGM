fffa8257901c3f6d7fe0c40e7bef548c78d00b3d
AnaGM2 <anagilmolina2@gmail.com>
2025-03-08 03:32:41 +0100

    Update __init__.py

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -3,6 +3,8 @@ from .agent import Agent
 from .on_policy_MC_all_visits import AgentMCOnPolicyAllVisits
 from .off_policy_MC_all_visits import AgentMCOffPolicyAllVisits
 from .SARSA import AgentSARSA
+from .Q-Learning import AgentQLearning
+from .Semi-gradient_SARSA import AgentSemiGradientSARSA
 
 # Lista de módulos o clases públicas
-__all__ = ['Agent', 'AgentMCOnPolicyAllVisits', 'AgentMCOffPolicyAllVisits', 'AgentSARSA']
+__all__ = ['Agent', 'AgentMCOnPolicyAllVisits', 'AgentMCOffPolicyAllVisits', 'AgentSARSA', 'AgentQLearning', 'AgentSemiGradientSARSA']
