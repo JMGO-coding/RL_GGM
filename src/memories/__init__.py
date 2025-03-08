d2127d04f8fa536557898f0bcfb91998bc4100c4
AnaGM2 <anagilmolina2@gmail.com>
2025-03-08 20:24:40 +0100

    Update __init__.py

--- src/memories/__init__.py
+++ src/memories/__init__.py
@@ -1,7 +1,5 @@
 # Importación de módulos o clases
-from .epsilon_soft import EpsilonSoftPolicy
-from .greedy_from_Q import GreedyFromQPolicy
-from .epsilon_greedy_continuous import EpsilonGreedyPolicyContinuous
+from .ReplayMemory import ReplayMemory
 
 # Lista de módulos o clases públicas
-__all__ = ['EpsilonSoftPolicy', 'GreedyFromQPolicy', 'EpsilonGreedyPolicyContinuous']
+__all__ = ['ReplayMemory']
