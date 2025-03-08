16006f575e9b243db4472d618856f76a50bf4e11
AnaGM2 <anagilmolina2@gmail.com>
2025-03-08 03:33:58 +0100

    Update __init__.py

--- src/wrappers/__init__.py
+++ src/wrappers/__init__.py
@@ -1,5 +1,6 @@
 # Importación de módulos o clases
 from .FrozenLake_custom_rewards import FrozenLakeCustomRewards
+from .TileCoding import TileCodingEnv
 
 # Lista de módulos o clases públicas
-__all__ = ['FrozenLakeCustomRewards']
+__all__ = ['FrozenLakeCustomRewards', 'TileCodingEnv']
