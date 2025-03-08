4a79fc163189d4179181d259e205d84f552e4325
AnaGM2 <anagilmolina2@gmail.com>
2025-03-08 21:08:20 +0100

    Update __init__.py

--- src/policies/__init__.py
+++ src/policies/__init__.py
@@ -2,6 +2,7 @@
 from .epsilon_soft import EpsilonSoftPolicy
 from .greedy_from_Q import GreedyFromQPolicy
 from .epsilon_greedy_continuous import EpsilonGreedyPolicyContinuous
+from .epsilon_greedy_DQN import EpsilonGreedyPolicyDQN
 
 # Lista de módulos o clases públicas
-__all__ = ['EpsilonSoftPolicy', 'GreedyFromQPolicy', 'EpsilonGreedyPolicyContinuous']
+__all__ = ['EpsilonSoftPolicy', 'GreedyFromQPolicy', 'EpsilonGreedyPolicyContinuous', 'EpsilonGreedyPolicyDQN']
