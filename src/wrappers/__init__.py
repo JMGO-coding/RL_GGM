99adcc135d1114e65b38348cf26a53660d6ccffc
AnaGM2 <anagilmolina2@gmail.com>
2025-03-09 19:15:33 +0100

    Update __init__.py

--- src/wrappers/__init__.py
+++ src/wrappers/__init__.py
@@ -2,6 +2,7 @@
 from .FrozenLake_custom_rewards import FrozenLakeCustomRewards
 from .TileCoding import TileCodingEnv
 from .MountainCar_custom_rewards import MountainCarCustomRewards
+from .CliffWalking_custom_rewards import CliffWalkingCustomRewards
 
 # Lista de módulos o clases públicas
-__all__ = ['FrozenLakeCustomRewards', 'TileCodingEnv', 'MountainCarCustomRewards']
+__all__ = ['FrozenLakeCustomRewards', 'TileCodingEnv', 'MountainCarCustomRewards', 'CliffWalkingCustomRewards']
